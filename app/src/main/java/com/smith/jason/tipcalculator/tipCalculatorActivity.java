01d7fe73f0322dbaa6a0778c7d414e242288b367
Jason <jasonbsmith1568@yahoo.com>
2015-08-05 19:24:23 -0700

    yea

--- app/src/main/java/com/smith/jason/tipcalculator/tipCalculatorActivity.java
+++ app/src/main/java/com/smith/jason/tipcalculator/tipCalculatorActivity.java
@@ -2,36 +2,60 @@ package com.smith.jason.tipcalculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.view.View.OnClickListener;
+import android.widget.TextView.OnEditorActionListener;
 
-public class tipCalculatorActivity extends AppCompatActivity {
+public class tipCalculatorActivity extends AppCompatActivity
+implements OnEditorActionListener, OnClickListener{
+
+    //define variables for the widgets
+    private TextView percentTextView;
+    private EditText billAmountEditText;
+    private TextView tipTextView;
+    private TextView totalTextView;
+    private Button percentUpButton;
+    private Button percentDownButton;
+    private Button resetButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tip_calculator);
+
+
+        //get references to the widgets
+        billAmountEditText = (EditText) findViewById(R.id.billAmountEditText);
+        percentTextView = (TextView) findViewById(R.id.percentTextView);
+        percentUpButton = (Button) findViewById(R.id.percentUpButton);
+        percentDownButton = (Button) findViewById(R.id.percentDownButton);
+        tipTextView = (TextView) findViewById(R.id.tipPercentView);
+        totalTextView = (TextView) findViewById(R.id.totalTextView);
+        resetButton = (Button) findViewById(R.id.resetButton);
+
+        //set the listeners
+        billAmountEditText.setOnEditorActionListener(this);
+        percentTextView.setOnEditorActionListener(this);
+        tipTextView.setOnEditorActionListener(this);
+        totalTextView.setOnEditorActionListener(this);
+        percentUpButton.setOnClickListener(this);
+        percentUpButton.setOnClickListener(this);
+        resetButton.setOnClickListener(this);
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_tip_calculator, menu);
-        return true;
+    public void onClick(View v) {
+
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+        return false;
     }
 }
